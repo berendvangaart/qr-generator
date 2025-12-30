70776589d0a0000fd1f84a10b9f9ae7501ebdbaa
Berend van Gaart <berendvangaart@gmail.com>
2025-12-30 15:54:30 +0100

    add watermark to qr

--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"strconv"
 
+	"github.com/nfnt/resize"
 	qrcode "github.com/skip2/go-qrcode"
 )
 
 func handleRequest(writer http.ResponseWriter, request *http.Request) {
 	request.ParseMultipartForm(10 << 20)
-
 	var size, content string = request.FormValue("size"), request.FormValue("content")
 	var codeData []byte
-
-	writer.Header().Set("Content-Type", "application/json")
 
 	if content == "" {
 		writer.WriteHeader(400)
@@ -28,16 +33,56 @@ func handleRequest(writer http.ResponseWriter, request *http.Request) {
 	qrCodeSize, err := strconv.Atoi(size)
 	if err != nil || size == "" {
 		writer.WriteHeader(400)
-		json.NewEncoder(writer).Encode("Could not determine the desired QR code size.")
+		json.NewEncoder(writer).Encode(
+			"Could not determine the desired QR code size.",
+		)
 		return
 	}
 
 	qrCode := simpleQRCode{Content: content, Size: qrCodeSize}
-	codeData, err = qrCode.Generate()
+
+	watermarkFile, _, err := request.FormFile("watermark")
+	if err != nil && errors.Is(err, http.ErrMissingFile) {
+		codeData, err = qrCode.Generate()
+		if err != nil {
+			writer.WriteHeader(400)
+			json.NewEncoder(writer).Encode(
+				fmt.Sprintf("Could not generate QR code. %v", err),
+			)
+			return
+		}
+		writer.Header().Add("Content-Type", "image/png")
+		writer.Write(codeData)
+		return
+	}
+
+	watermark, err := uploadFile(watermarkFile)
 	if err != nil {
 		writer.WriteHeader(400)
 		json.NewEncoder(writer).Encode(
-			fmt.Sprintf("Could not generate QR code. %v", err),
+			fmt.Sprint("Could not upload the watermark image.", err),
+		)
+		return
+	}
+
+	contentType := http.DetectContentType(watermark)
+	if err != nil {
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode(
+			fmt.Sprintf(
+				"Provided watermark image is a %s not a PNG. %v.", err, contentType,
+			),
+		)
+		return
+	}
+
+	codeData, err = qrCode.GenerateWithWatermark(watermark)
+	if err != nil {
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode(
+			fmt.Sprintf(
+				"Could not generate QR code with the watermark image. %v", err,
+			),
 		)
 		return
 	}
@@ -51,6 +96,28 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+func uploadFile(file multipart.File) ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, file); err != nil {
+		return nil, fmt.Errorf("could not upload file. %v", err)
+	}
+
+	return buf.Bytes(), nil
+}
+
+func resizeWatermark(watermark io.Reader, width uint) ([]byte, error) {
+	decodedImage, err := png.Decode(watermark)
+	if err != nil {
+		return nil, fmt.Errorf("could not decode watermark image: %v", err)
+	}
+
+	m := resize.Resize(width, 0, decodedImage, resize.Lanczos3)
+	resized := bytes.NewBuffer(nil)
+	png.Encode(resized, m)
+
+	return resized.Bytes(), nil
+}
+
 type simpleQRCode struct {
 	Content string
 	Size    int
@@ -62,4 +129,59 @@ func (code *simpleQRCode) Generate() ([]byte, error) {
 		return nil, fmt.Errorf("could not generate a QR code: %v", err)
 	}
 	return qrCode, nil
+}
+
+func (code *simpleQRCode) GenerateWithWatermark(watermark []byte) ([]byte, error) {
+	qrCode, err := code.Generate()
+	if err != nil {
+		return nil, err
+	}
+
+	qrCode, err = code.addWatermark(qrCode, watermark)
+	if err != nil {
+		return nil, fmt.Errorf("could not add watermark to QR code: %v", err)
+	}
+
+	return qrCode, nil
+}
+
+func (code *simpleQRCode) addWatermark(qrCode []byte, watermarkData []byte) ([]byte, error) {
+	qrCodeData, err := png.Decode(bytes.NewBuffer(qrCode))
+	if err != nil {
+		return nil, fmt.Errorf("could not decode QR code: %v", err)
+	}
+
+	watermarkWidth := uint(float64(qrCodeData.Bounds().Dx()) * 0.25)
+	watermark, err := resizeWatermark(bytes.NewBuffer(watermarkData), watermarkWidth)
+	if err != nil {
+		return nil, fmt.Errorf("Could not resize the watermark image.", err)
+	}
+
+	watermarkImage, err := png.Decode(bytes.NewBuffer(watermark))
+	if err != nil {
+		return nil, fmt.Errorf("could not decode watermark: %v", err)
+	}
+
+	var halfQrCodeWidth, halfWatermarkWidth int = qrCodeData.Bounds().Dx() / 2, watermarkImage.Bounds().Dx() / 2
+	offset := image.Pt(
+		halfQrCodeWidth-halfWatermarkWidth,
+		halfQrCodeWidth-halfWatermarkWidth,
+	)
+
+	watermarkImageBounds := qrCodeData.Bounds()
+	m := image.NewRGBA(watermarkImageBounds)
+
+	draw.Draw(m, watermarkImageBounds, qrCodeData, image.Point{}, draw.Src)
+	draw.Draw(
+		m,
+		watermarkImage.Bounds().Add(offset),
+		watermarkImage,
+		image.Point{},
+		draw.Over,
+	)
+
+	watermarkedQRCode := bytes.NewBuffer(nil)
+	png.Encode(watermarkedQRCode, m)
+
+	return watermarkedQRCode.Bytes(), nil
 }
